06283aec57b0e135efffa2ffe1bbb46dede1bab9
Naidu <naidup.developer@gmail.com>
2022-07-29 23:55:37 +0530

    basic animation

--- CoreAnimationDemo/ViewController.swift
+++ CoreAnimationDemo/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 100))
+        myView.frame = frame
+        myView.backgroundColor = .black
+        self.view.addSubview(myView)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let animation = CABasicAnimation(keyPath: "position.x")
+        animation.fromValue = CGPoint.zero
+        animation.toValue = view.bounds.size.width
+        animation.duration = 0.5
+        animation.beginTime = CACurrentMediaTime() + 0.3
+        animation.repeatCount = 4
+        animation.autoreverses = true
+        
+        myView.layer.add(animation, forKey: nil)
+    }
 
 }
 
